f3ab506447c387098a51dcc4722f24ff9b6a86e4
Luke Collins <luke.a.collins@outlook.com>
2021-11-25 15:58:04 -0800

    feat: Generalize docopt pointer, add gitignores
    Generalize docopt pointer, add gitignores to raw and preprocessed dirs

--- data/download_datasets.py
+++ data/download_datasets.py
@@ -4,15 +4,22 @@
 """This script downloads the csv files specified in files_to_download.txt
 Usage: download_datasets.py [<output_dir>]
 Options:
-[<output_dir>]    Takes a directory to which dataset ought to be downloaded (optional)
+[<output_dir>]  Directory to which datasets should be downloaded [default: './data/raw']
 """ 
-
-from docopt import docopt
+import importlib.util
+import os
 import requests
-opt = docopt(__doc__)
 
-def main(output_dir=''):
-    with open('files_to_download.txt') as f:
+os.chdir('..')
+print(os.getcwd())
+spec = importlib.util.spec_from_file_location("docopt", "docopt.py")
+docopt = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(docopt)
+
+opt = docopt.docopt(__doc__)
+
+def main(output_dir):
+    with open('data/files_to_download.txt') as f:
         files = f.read().splitlines()
     
     number_of_files = len(files)
@@ -22,10 +29,11 @@ def main(output_dir=''):
         
         req = requests.get(file)
 
+        print(opt[output_dir])
         if opt[output_dir] is not None:
             csv_file = open(opt[output_dir] + filename, 'wb')
         else:
-            csv_file = open(filename, 'wb')
+            csv_file = open('data/raw/' + filename, 'wb')
         
         csv_file.write(req.content)
         csv_file.close()
