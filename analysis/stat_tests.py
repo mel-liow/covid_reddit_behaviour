444a7b3982092f8f33e74d40731bbc590985a4b5
Luke Collins <luke.a.collins@outlook.com>
2021-11-25 17:42:38 -0800

    fix: repoint docopt.py reference.
    fix: repoint docopt.py reference.

--- analysis/stat_tests.py
+++ analysis/stat_tests.py
@@ -6,18 +6,23 @@ This script runs preprocessed data through Wilcoxon rank-sum tests
 for significance of difference of median substance_use_total
 between 'pre' and 'post' datasets.
 
-Usage: stat_tests.py --data_path = <dir> --output = <file>
+Usage: stat_tests.py --data_path=<dir> [--output=<file>]
 Options:
 --data_path=<dir>  path to dir in which preprocessed data is stored
---output=<file>  file to write csv output [default: './stat_tests_output.csv']
+--output=<file>    file to write csv output [default: './analysis/stat_tests/stat_tests_output.csv']
 """
 
-from docopt import docopt
 import pandas as pd
 from scipy.stats import ranksums
 import glob
+import importlib.util
+import os
+os.chdir('..') # this should always be '..' I think
+spec = importlib.util.spec_from_file_location("docopt", "docopt.py")
+docopt = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(docopt)
 
-opt = docopt(__doc__)
+opt = docopt.docopt(__doc__)
 
 def main(data_path, output_file):
     """
@@ -33,15 +38,17 @@ def main(data_path, output_file):
     None
     """
     # read the datasets
-    preprocessed_data = [datafile.split('\\')[-1] for datafile in glob.glob(data_path + '\*.csv')] # '..\..\data\*.csv'
-    pre_datasets = [x for x in preprocessed_data if 'pre' in x]
-    post_datasets = [x for x in preprocessed_data if 'post' in x]
+    preprocessed_data = [datafile.split('\\')[-1] for datafile in glob.glob(data_path + '/*.csv')] # '..\..\data\*.csv'
+    pre_datasets = [x for x in preprocessed_data if '_pre_' in x]
+    post_datasets = [y for y in preprocessed_data if '_post_' in y]
+    print(pd.Series(pre_datasets))
+    print(pd.Series(post_datasets))
 
     results = {}
-    for i in range(len(pre_datasets)):
+    for i, file in enumerate(pre_datasets):
         # analysis\preprocessing\*.csv
-        pre = pd.read_csv('../../data/%s' % pre_datasets[i])['substance_use_total']
-        post = pd.read_csv('../../data/%s' % post_datasets[i])['substance_use_total']
+        pre = pd.read_csv(data_path + file)['substance_use_total']
+        post = pd.read_csv(data_path + post_datasets[i])['substance_use_total']
         test_statistic, p_val = ranksums(pre, post, alternative='two-sided')
         results[pre_datasets[i].split('_')[0]] = {'test_statistic': test_statistic,
                                                   'p_value': p_val}
@@ -50,6 +57,6 @@ def main(data_path, output_file):
 
 
 if __name__ == "__main__":
-    data_path = opt["<dir>"]
-    output_file = opt["<file>"]
+    data_path = opt["--data_path"]
+    output_file = opt["--output"]
     main(data_path, output_file)
